c3eb1d3d205d6a56a11cf0d42efe0b9049e03c08
Ayenar <pretty.v4cant@gmail.com>
2020-09-20 22:12:58 +0200

    Architektura

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AX",
+            "BX",
+            "CX",
+            "DX"});
+            this.comboBox1.Location = new System.Drawing.Point(24, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "AX",
+            "BX",
+            "CX",
+            "DX"});
+            this.comboBox2.Location = new System.Drawing.Point(24, 186);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(158, 24);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(80, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Źródło";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(75, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rejestr";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(356, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(356, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(356, 187);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(144, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(329, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "AX";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(329, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "BX";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(329, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "CX";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(329, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "DX";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(373, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 35);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Wykonaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 308);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(476, 130);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(610, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 77);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Wyczyść wartości";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Emulator MOV Intel 8086";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string liczba1;
+        string liczba2;
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string c = textBox1.Text;
+            string d = textBox2.Text;
+            string dd = textBox3.Text;
+            string ddd = textBox4.Text;
+
+
+
+
+            //rejestr AX
+            if (comboBox1.Text == "AX" && comboBox2.Text == "BX")
+            {
+                textBox1.Text = c;
+                textBox2.Text = textBox1.Text;
+                MessageBox.Show("Nowa wartość rejestru BX to " + c);
+                richTextBox1.Text = ("BX przyjął wartość " + c);
+             
+            }
+
+
+            else if (comboBox1.Text == "AX" && comboBox2.Text == "CX")
+            {
+                textBox1.Text = c;
+                textBox3.Text = textBox1.Text;
+                MessageBox.Show("Nowa wartość rejestru CX to " + c);
+                richTextBox1.Text = ("CX przyjął wartość " + c);
+
+            }
+
+            else if (comboBox1.Text == "AX" && comboBox2.Text == "DX")
+            {
+                textBox1.Text = c;
+                textBox4.Text = textBox1.Text;
+                MessageBox.Show("Nowa wartość rejestru DX to " + c);
+                richTextBox1.Text = ("DX przyjął wartość " + c);
+
+            }
+
+
+            //rejestr BX
+            else if (comboBox1.Text == "BX" && comboBox2.Text == "AX")
+            {
+                textBox2.Text = d;
+                textBox1.Text = textBox2.Text;
+                MessageBox.Show("Nowa wartość rejestru AX to " + d);
+                richTextBox1.Text = ("AX przyjął wartość " + d);
+
+            }
+
+            else if (comboBox1.Text == "BX" && comboBox2.Text == "CX")
+            {
+                textBox2.Text = d;
+                textBox3.Text = textBox2.Text;
+
+                MessageBox.Show("Nowa wartość rejestru CX to " + d);
+                richTextBox1.Text = ("CX przyjął wartość " + d);
+            }
+
+            else if (comboBox1.Text == "BX" && comboBox2.Text == "DX")
+            {
+                textBox2.Text = d;
+                textBox4.Text = textBox2.Text;
+
+                MessageBox.Show("Nowa wartość rejestru DX to " + d);
+                richTextBox1.Text = ("DX przyjął wartość " + d);
+            }
+
+            //rejestr CX
+            else if (comboBox1.Text == "CX" && comboBox2.Text == "AX")
+            {
+                textBox3.Text = dd;
+                textBox1.Text = textBox3.Text;
+                MessageBox.Show("Nowa wartość rejestru AX to " + dd);
+                richTextBox1.Text = ("AX przyjął wartość " + dd);
+
+            }
+
+            else if (comboBox1.Text == "CX" && comboBox2.Text == "BX")
+            {
+                textBox3.Text = dd;
+                textBox2.Text = textBox3.Text;
+                MessageBox.Show("Nowa wartość rejestru BX to " + dd);
+                richTextBox1.Text = ("BX przyjął wartość " + dd);
+
+            }
+
+            else if (comboBox1.Text == "CX" && comboBox2.Text == "DX")
+            {
+                textBox3.Text = dd;
+                textBox4.Text = textBox3.Text;
+                MessageBox.Show("Nowa wartość rejestru DX to " + dd);
+                richTextBox1.Text = ("DX przyjął wartość " + dd);
+
+            }
+
+            //rejestr D
+            else if (comboBox1.Text == "DX" && comboBox2.Text == "AX")
+            {
+                textBox4.Text = ddd;
+                textBox1.Text = textBox4.Text;
+                MessageBox.Show("Nowa wartość rejestru AX to " + ddd);
+                richTextBox1.Text = ("AX przyjął wartość " + ddd);
+
+            }
+            else if (comboBox1.Text == "DX" && comboBox2.Text == "BX")
+            {
+                textBox4.Text = ddd;
+                textBox2.Text = textBox4.Text;
+                MessageBox.Show("Nowa wartość rejestru BX to " + ddd);
+                richTextBox1.Text = ("BX przyjął wartość " + ddd);
+
+            }
+
+            else if (comboBox1.Text == "DX" && comboBox2.Text == "CX")
+            {
+
+                textBox3.Text = textBox4.Text;
+                MessageBox.Show("Nowa wartość rejestru CX to " + ddd);
+                richTextBox1.Text = ("CX przyjął wartość " + ddd);
+            }
+
+
+
+            //podwojne wartosci
+            else if (comboBox1.Text == "AX" && comboBox2.Text == "AX")
+            {
+                MessageBox.Show("Rejestr i źródło muszą być różne");
+            }
+
+            else if (comboBox1.Text == "BX" && comboBox2.Text == "BX")
+            {
+                MessageBox.Show("Rejestr i źródło muszą być różne");
+            }
+
+            else if (comboBox1.Text == "CX" && comboBox2.Text == "DX")
+            {
+                MessageBox.Show("Rejestr i źródło muszą być różne");
+            }
+
+            else if (comboBox1.Text == "DX" && comboBox2.Text == "DX")
+            {
+                MessageBox.Show("Rejestr i źródło muszą być różne");
+            }
+
+
+            else
+
+            {
+                MessageBox.Show("Wybierz źródło oraz rejestr");
+            }
+
+
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+
+        }
+    }
+}
